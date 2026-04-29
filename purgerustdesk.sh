44bd919289856f528865f62829776a1c8f0e7755
TylerJP <33613789+TJP507@users.noreply.github.com>
2026-04-28 21:18:45 -0400

    Create purgerustdesk.sh to remove RustDesk and GoHTTP
    This script stops and removes RustDesk and GoHTTP services along with their associated files and logs.

---
+++ purgerustdesk.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+sudo systemctl stop rustdeskrelay.service rustdesksignal.service
+sudo systemctl disable rustdeskrelay.service rustdesksignal.service
+sudo rm -rf /var/log/rustdesk/
+sudo rm -rf /opt/rustdesk/
+sudo rm -rf /etc/systemd/system/rustdesksignal.service
+sudo rm -rf /etc/systemd/system/rustdeskrelay.service
+
+sudo systemctl stop gohttpserver.service
+sudo systemctl disable gohttpserver.service
+sudo rm -rf /var/log/gohttp/
+sudo rm -rf /opt/gohttp/
+sudo rm -rf /etc/systemd/system/gohttpserver.service
